692a540ce717b5c6ca46f847415bf39f38045327
shizuketsu <evdokimovnikita501@gmail.com>
2024-10-06 09:19:43 +0300

    init project

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="container">
+                <div className="header-body">
+                    <div className="logo"></div>
+                    <nav className="header-menu" id="menu"></nav>
+                    <div className="hamburger"></div>
+                </div>
+            </div>
+        </header>
+    );
+}
